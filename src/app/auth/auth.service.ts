a86d1a3fd3b34008acf26f93f08513c1684953b8
amabelle <amabelletr@gmail.com>
2021-08-24 22:27:34 +0300

    Displaying an error msg

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -32,8 +32,28 @@ export class AuthService {
           password: password,
           returnSecureToken: true
         }
+      ).pipe(
+        catchError(this.handleError)
       );
   }
 
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct.';
+        break;
+    }
+    return throwError(errorMessage);
+  }
 
 }
