7567f6072011d008646e1e6bb1ff7303fc2009ff
amabelle <amabelletr@gmail.com>
2021-08-24 19:36:08 +0300

    Adding onSwitch, loging submition to console

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -14,5 +14,20 @@ export class AuthComponent {
   isLoading = false;
   error: string = null;
 
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    console.log(form.value);
+
+    form.reset();
+  }
 
 }
