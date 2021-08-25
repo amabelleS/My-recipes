8383d39d2b929b1146431cb539a0b65424267860
amabelle <amabelletr@gmail.com>
2021-08-25 21:10:17 +0300

    Recipes routing added

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RecipesRoutingModule } from './recipes-routing.module';
 
 import { RecipesComponent } from './recipes.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
@@ -9,8 +10,6 @@ import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
-// import { RecipesRoutingModule } from './recipes-routing.module';
-// import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -24,15 +23,8 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
   imports: [
     RouterModule,
     CommonModule,
-    ReactiveFormsModule
-  ],
-  exports: [
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeStartComponent,
-    RecipeEditComponent
+    ReactiveFormsModule,
+    RecipesRoutingModule
   ]
 })
 export class RecipesModule {}
